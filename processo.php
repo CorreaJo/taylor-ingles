58425c165e7eb0bc64835c522a00569d9d5fa40b
João Victor Corrêa <75680576+CorreaJo@users.noreply.github.com>
2022-04-21 23:31:36 -0300

    Update processo.php

--- processo.php
+++ processo.php
@@ -2,42 +2,43 @@
     $albuns = $_POST["album"];
 
         if($albuns == "taylor swift"){
-            header("Location: taylor.html", true, 301);
+            header("Location: https://correajo.github.io/taylor-ingles/taylor.html", true, 301);
             exit();
         }
         if($albuns == "fearless"){
-            header("Location: fearless.html", true, 301);
+            header("Location: https://correajo.github.io/taylor-ingles/fearless.html", true, 301);
             exit();
         }
         if($albuns == "speak now"){
-            header("Location: speaknow.html", true, 301);
+            header("Location: https://correajo.github.io/taylor-ingles/speaknow.html", true, 301);
             exit();
         }
         if($albuns == "red"){
-            header("Location: red.html", true, 301);
+            header("Location: https://correajo.github.io/taylor-ingles/red.html.html", true, 301);
             exit();
         }
         if($albuns == "1989"){
-            header("Location: 1989.html", true, 301);
+            header("Location: https://correajo.github.io/taylor-ingles/1989.html", true, 301);
             exit();
         }
         if($albuns == "reputation"){
-            header("Location: reputation.html", true, 301);
+            header("Location: https://correajo.github.io/taylor-ingles/reputation.html", true, 301);
             exit();
         }
         if($albuns == "lover"){
-            header("Location: lover.html", true, 301);
+            header("Location: https://correajo.github.io/taylor-ingles/lover.html", true, 301);
             exit();
         }
         if($albuns == "folklore"){
-            header("Location: folklore.html", true, 301);
+            header("Location: https://correajo.github.io/taylor-ingles/folklore.html", true, 301);
             exit();
         }
         if($albuns == "evermore"){
-            header("Location: evermore.html", true, 301);
+            header("Location: https://correajo.github.io/taylor-ingles/evermore.html", true, 301);
             exit();
         } else {
-            header("Location: erro.html", true, 301);
+            header("Location: https://correajo.github.io/taylor-ingles/erro.html", true, 301);
             exit();
         }
 ?>
+
